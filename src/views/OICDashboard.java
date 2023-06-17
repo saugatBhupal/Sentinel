a538eb36550f498a0ce49ba08ddda97859719c78
Kabita <mahatokk008@gmail.com>
2023-06-17 19:48:35 +0545

    update oic dashboard

--- src/views/OICDashboard.java
+++ src/views/OICDashboard.java
@@ -48,3 +48,102 @@ public class OICDashboard extends JFrame {
         this.policeController = new PoliceControllerImpl(panel);
         initialize();
     }
+     public void initialize() {
+
+        frame = new JFrame();
+        frame.setBounds(0, 0, 1201, 841);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setLayout(null);
+
+        panel = new JPanel();
+        panel.setBackground(new Color(254, 254, 254, 1));
+        panel.setBounds(0, 0, 1201, 841);
+        panel.setLayout(null);
+        frame.getContentPane().add(panel);
+
+        JLabel logo = new JLabel();
+        logo.setBounds(25, 35, 58, 57);
+        logo.setIcon(imagePlugins.resize(new ImageIcon("resources/artboards/Sentinel-logo-2.png").getImage(), logo));
+        panel.add(logo);
+
+        JLabel parentPageTitle = new JLabel();
+        parentPageTitle.setText("F.I.R");
+        parentPageTitle.setFont(new Font("Jost", Font.PLAIN, 20));
+        parentPageTitle.setForeground(Color.decode("#002349"));
+        parentPageTitle.setBounds(239, 61, 141, 32);
+        panel.add(parentPageTitle);
+
+        DateTimeWidget.addWidget(panel);
+
+        JLabel dashboardIcon = new JLabel();
+        dashboardIcon.setBounds(36, 125, 35, 35);
+        dashboardIcon.setIcon(new ImageIcon("resources/artboards/dash-icon-default.png"));
+        panel.add(dashboardIcon);
+
+        JLabel recordsIcon = new JLabel();
+        recordsIcon.setBounds(39, 285, 35, 35);
+        recordsIcon.setIcon(imagePlugins
+                .resize(new ImageIcon("resources/artboards/records-icon-default.png").getImage(), recordsIcon));
+        panel.add(recordsIcon);
+
+        JLabel officersIcon = new JLabel();
+        officersIcon.setBounds(44, 349, 35, 25);
+        officersIcon.setIcon(imagePlugins
+                .resize(new ImageIcon("resources/artboards/police-icon-selected.png").getImage(), officersIcon));
+        panel.add(officersIcon);
+
+        JLabel citizensIcon = new JLabel();
+        citizensIcon.setBounds(39, 413, 35, 35);
+        citizensIcon.setIcon(new ImageIcon("resources/artboards/citizen-icon-default.png"));
+        panel.add(citizensIcon);
+
+        JLabel convictsIcon = new JLabel();
+        convictsIcon.setBounds(39, 477, 35, 35);
+        convictsIcon.setIcon(new ImageIcon("resources/artboards/convict-icon-default.png"));
+        panel.add(convictsIcon);
+
+        JLabel sideMenuBar = new JLabel();
+        sideMenuBar.setBounds(0, 0, 108, 841);
+        sideMenuBar.setBackground(Color.decode("#002349"));
+        sideMenuBar.setOpaque(true);
+        panel.add(sideMenuBar);
+
+        JLabel FIRsIcon = new JLabel();
+        FIRsIcon.setBounds(278, 264, 53, 53);
+        FIRsIcon.setOpaque(true);
+        FIRsIcon.setIcon(
+                imagePlugins.resize(new ImageIcon("resources/artboards/all-FIRs.png").getImage(), FIRsIcon));
+        panel.add(FIRsIcon);
+
+        JLabel FIRsLine = new JLabel();
+        FIRsLine.setBounds(370, 254, 1, 100);
+        FIRsLine.setBackground(Color.decode("#1A75D5"));
+        FIRsLine.setOpaque(true);
+        panel.add(FIRsLine);
+
+        JLabel FIRsText = new JLabel();
+        FIRsText.setFont(new Font("Jost", Font.PLAIN, 14));
+        FIRsText.setForeground(Color.decode("#002349"));
+        FIRsText.setBounds(294, 311, 142, 55);
+        FIRsText.setText("F.I.R");
+        panel.add(FIRsText);
+
+        JTextArea FIRsDescription = new JTextArea();
+        FIRsDescription.setBounds(394, 264, 150, 78);
+        FIRsDescription.setText(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eleifend.");
+        FIRsDescription.setForeground(Color.decode("#002349"));
+        FIRsDescription.setEditable(false);
+        FIRsDescription.setLineWrap(true);
+        FIRsDescription.setWrapStyleWord(true);
+        font = FIRsText.getFont();
+        FIRsDescription.setFont(font);
+        panel.add(FIRsDescription);
+
+        JLabel policeIcon = new JLabel();
+        policeIcon.setBounds(765, 264, 53, 53);
+        policeIcon.setOpaque(true);
+        policeIcon.setIcon(
+                imagePlugins.resize(new ImageIcon("resources/artboards/new-FIR.png").getImage(), policeIcon));
+        panel.add(policeIcon);
