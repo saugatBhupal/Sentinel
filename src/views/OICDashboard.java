52888609236480d4adf7ebb5f35e99c8633ad190
Kabita <mahatokk008@gmail.com>
2023-06-17 19:47:54 +0545

    update oic dashboard

--- src/views/OICDashboard.java
+++ src/views/OICDashboard.java
@@ -1,5 +1,50 @@
 package views;
 
-public class OICDashboard {
-    
-}
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import org.apache.batik.gvt.text.GVTAttributedCharacterIterator.TextAttribute;
+
+import controller.CitizenController;
+import controller.PoliceController;
+import controller.controllerImpl.CitizenControllerImpl;
+import controller.controllerImpl.PoliceControllerImpl;
+import model.Citizen;
+import model.Police;
+import plugins.MediaFormat;
+import plugins.ImagePlugins.ImagePlugins;
+import plugins.PluginFactory.PluginFactory;
+import utils.dateTime.DateTimeUtil;
+import utils.generator.PasswordGenerator;
+import utils.ui.event.Focus;
+import utils.ui.event.Hover;
+import views.widget.DateTimeWidget;
+
+public class OICDashboard extends JFrame {
+    private final CitizenController citizenController;
+    private final PoliceController policeController;
+    private ImagePlugins imagePlugins = PluginFactory.createPlugin(MediaFormat.ofType.IMAGE);
+    private JFrame frame;
+    private JPanel panel;
+    private Font font;
+    private Map<TextAttribute, Object> attributes;
+
+    public OICDashboard() {
+        this.citizenController = new CitizenControllerImpl();
+        this.policeController = new PoliceControllerImpl(panel);
+        initialize();
+    }
