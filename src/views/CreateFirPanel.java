088332b10e9471432285742a18c208bf0f1bf7fa
Aastha <aasthadahal312@gmail.com>
2023-06-17 19:26:51 +0545

    Fir panel updated

--- src/views/CreateFirPanel.java
+++ src/views/CreateFirPanel.java
@@ -1,5 +1,43 @@
 package views;
 
-public class CreateFirPanel {
-    
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Date;
+import java.sql.Time;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import controller.FirController;
+import controller.controllerImpl.FirControllerImpl;
+import model.Fir;
+import model.Police;
+import plugins.MediaFormat;
+import plugins.ImagePlugins.ImagePlugins;
+import plugins.PluginFactory.PluginFactory;
+import utils.ui.event.Focus;
+import utils.ui.event.Hover;
+import views.widget.DateTimeWidget;
+
+public class CreateFirPanel extends JFrame {
+    private ImagePlugins imagePlugins = PluginFactory.createPlugin(MediaFormat.ofType.IMAGE);
+    private final FirController firController;
+    private JFrame frame;
+    private JPanel panel;
+
+    public CreateFirPanel() {
+        initialize();
+        this.firController = new FirControllerImpl();
+    }
 }
+
+   
