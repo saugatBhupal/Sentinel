4511148ffb4f9ed74e4a00a74240b380643e431c
saugat singh <saugatbhupal@bhunet.com>
2023-06-28 23:03:46 +0545

    Created Fit entity and fir controllers

---
+++ src/model/Fir.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Fir {
+
+    @NonNull
+    private Long firID;
+    
+    private Long filedBy;
+    private Long filedAgainst;
+    private Long witness;
+
+    @NonNull
+    private Date filedDate;
+
+    @NonNull
+    private Time filedTime;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    private String category;
+
+    private String evidence;
+
+    @NonNull
+    private Long registeredBy;
+
+    @NonNull
+    private Integer status;
+
+    private Citizen victimCitizen;
+    private Citizen offenderCitizen;
+    private Police police;
+}
